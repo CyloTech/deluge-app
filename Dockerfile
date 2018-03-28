942e06e1a241530b301a839844d69daa7eb27670
Rid <grant@seednet.eu>
2018-03-28 11:49:15 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,65 @@
+FROM alpine:3.7
+
+ENV PYTHON_EGG_CACHE="/torrents/config/deluge/plugins/.python-eggs"
+
+RUN \
+  addgroup -S deluge -g 1000 && \
+  adduser -D -S -h /home/deluge -s /sbin/nologin -G deluge deluge -u 1000 && \
+  echo "**** install build packages ****" && \
+ apk add --no-cache --virtual=build-dependencies \
+	g++ \
+	gcc \
+	libffi-dev \
+	openssl-dev \
+	py2-pip \
+	python2-dev && \
+ echo "**** install runtime packages ****" && \
+ apk add --no-cache \
+    supervisor \
+    bash \
+	ca-certificates \
+	curl \
+	libressl2.6-libssl \
+	openssl \
+	p7zip \
+	unrar \
+	libcap \
+	unzip && \
+ apk add --no-cache \
+	--repository http://nl.alpinelinux.org/alpine/edge/testing \
+	deluge && \
+ echo "**** install pip packages ****" && \
+ pip install --no-cache-dir -U \
+	incremental \
+	pip && \
+ pip install --no-cache-dir -U \
+	crypto \
+	mako \
+	markupsafe \
+	pyopenssl \
+	service_identity \
+	six \
+	twisted \
+	zope.interface && \
+ echo "**** cleanup ****" && \
+ apk del --purge \
+	build-dependencies && \
+ rm -rf /root/.cache && \
+ wget -q http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz && \
+ gunzip GeoIP.dat.gz && \
+ mkdir -p /usr/share/GeoIP && \
+ mv GeoIP.dat /usr/share/GeoIP/GeoIP.dat
+
+# add local files
+
+RUN setcap cap_net_bind_service=+ep /usr/bin/python2.7
+RUN mkdir -p /torrents/config/deluge
+ADD sources /sources
+ADD sources/supervisord.conf /etc/supervisord.conf
+ADD scripts/deluge-pass.py /scripts/deluge-pass.py
+ADD scripts/start.sh /scripts/start.sh
+RUN chmod -R +x /scripts
+# ports
+EXPOSE 80 58846 58946 58946/udp
+
+CMD [ "/scripts/start.sh" ]
---
+++ scripts/start.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+set -x
+
+###########################[ SUPERVISOR SCRIPTS ]###############################
+
+if [ ! -f /etc/app_configured ]; then
+    mkdir -p /etc/supervisor/conf.d
+cat << EOF >> /etc/supervisor/conf.d/deluged.conf
+[program:deluged]
+command=/bin/su -s /bin/bash -c "TERM=xterm /usr/bin/deluged -c /torrents/config/deluge/ -d --loglevel=info -l /torrents/config/log/deluged.log" deluge
+autostart=true
+autorestart=true
+priority=1
+stdout_events_enabled=true
+stderr_events_enabled=true
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
+EOF
+
+cat << EOF >> /etc/supervisor/conf.d/deluge-web.conf
+[program:deluge-web]
+command=/bin/su -s /bin/bash -c "TERM=xterm /usr/bin/deluge-web -c /torrents/config/deluge/ --loglevel=info" deluge
+autostart=true
+autorestart=true
+priority=2
+stdout_events_enabled=true
+stderr_events_enabled=true
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
+EOF
+fi
+
+###########################[ DELUGE SETUP ]###############################
+
+if [ ! -f /etc/app_configured ]; then
+    mkdir -p /torrents/downloading
+    mkdir -p /torrents/completed
+    mkdir -p /torrents/config/deluge
+    mkdir -p /torrents/config/deluge/plugins
+    mkdir -p /torrents/config/deluge/plugins/.python-eggs
+    mkdir -p /torrents/config/log
+    mkdir -p /torrents/config/torrents
+    mkdir -p /torrents/watch
+
+    cp /sources/core.conf /torrents/config/deluge
+    cp /sources/web.conf /torrents/config/deluge
+
+    /scripts/deluge-pass.py /torrents/config/deluge ${DELUGE_PASSWORD}
+
+    cat /torrents/config/deluge/auth | grep "${DELUGE_USERNAME}" || echo "${DELUGE_USERNAME}:${DELUGE_PASSWORD}:10" >> /torrents/config/deluge/auth
+
+    chown -R deluge:deluge /torrents
+fi
+
+###########################[ MARK INSTALLED ]###############################
+
+if [ ! -f /etc/app_configured ]; then
+    touch /etc/app_configured
+    curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "https://api.cylo.io/v1/apps/installed/$INSTANCE_ID"
+fi
+
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
