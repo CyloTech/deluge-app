52dfe88d11bf9b6cfb26b41a8b23fc538c6fa3af
Rid <grant@seednet.eu>
2018-05-12 11:05:51 +0100

    Just replace ports on update

--- scripts/start.sh
+++ scripts/start.sh
@@ -62,7 +62,7 @@ if [ ! -f /torrents/config/deluge/core.conf ]; then
     sed -i 's#DAEMON_PORT#'${DAEMON_PORT}'#g' /torrents/config/deluge/hostlist.conf.1.2
 else
     sed -i 's#"daemon_port": [0-9]*,#"daemon_port": '${DAEMON_PORT}',#g' /torrents/config/deluge/core.conf
-    sed -i -z 's#"listen_ports": \[\n\s*[0-9]*,\s*[0-9]*\n\s*\],#"listen_ports": \[\n    '${FIRST_PORT}',\n    '${LAST_PORT}'\n  ],#g' /torrents/config/deluge/core.conf
+    sed -i -z -E 's#"listen_ports": \[\n(.|\s)*\],#"listen_ports": \[\n    '${FIRST_PORT}',\n    '${LAST_PORT}'\n  ],#g' /torrents/config/deluge/core.conf
     sed -i 's#127.0.0.1:[0-9]*#127.0.0.1:'${DAEMON_PORT}'#g' /torrents/config/deluge/web.conf
     sed -i -z 's#"127.0.0.1",\n\s*[0-9]*,#"127.0.0.1",\n      '${DAEMON_PORT}',#g' /torrents/config/deluge/hostlist.conf.1.2
 fi
